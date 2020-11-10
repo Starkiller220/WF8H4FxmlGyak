b1f8692ccc4800265393ec651132788154e9fe05
Starkiller220 <32932210+Starkiller220@users.noreply.github.com>
2020-11-10 16:19:10 +0100

    Commit 1110

---
+++ WF8H4F_1110/XpathModifyWF8H4F.java
@@ -0,0 +1,44 @@
+package WF8H4F_1110;
+
+import java.io.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+public class XpathModifyWF8H4F {
+    public static void main(String[] args)
+            throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File("WF8H4F_1110\\studentWF8H4F.xml"));
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xpath = xpathFactory.newXPath();
+
+        String expression = "/class/student";
+        NodeList nodes = (NodeList) xpath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            Element element = (Element) node;
+
+            String rollno = element.getAttribute("rollno");
+            System.out.println("\nCurrent element: " + node.getNodeName());
+            System.out.println("Student roll no: " + rollno);
+
+            NodeList childNodes = element.getChildNodes();
+
+            for (int j = 0; j < childNodes.getLength(); j++) {
+                Node childNode = childNodes.item(j);
+
+                if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                    if (rollno.equals("393") && childNode.getNodeName() == "lastname") {
+                        childNode.setTextContent("Zöld");
+                    }
+
+                    System.out.println(childNode.getNodeName() + ": " + childNode.getTextContent());
+                }
+            }
+        }
+    }
+}
---
+++ WF8H4F_1110/XpathQueryWF8H4F.java
@@ -0,0 +1,39 @@
+package WF8H4F_1110;
+
+import java.io.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+public class XpathQueryWF8H4F {
+    public static void main(String[] args)
+            throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File("WF8H4F_1110\\studentWF8H4F.xml"));
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xpath = xpathFactory.newXPath();
+
+        String expression = "/class/student[@rollno='593']";
+        NodeList nodes = (NodeList) xpath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            Element element = (Element) node;
+
+            System.out.println("\nCurrent element: " + node.getNodeName());
+            System.out.println("Student roll no: " + element.getAttribute("rollno"));
+
+            NodeList childNodes = element.getChildNodes();
+
+            for (int j = 0; j < childNodes.getLength(); j++) {
+                Node childNode = childNodes.item(j);
+
+                if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                    System.out.println(childNode.getNodeName() + ": " + childNode.getTextContent());
+                }
+            }
+        }
+    }
+}
